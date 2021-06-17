69690dfaeb1fe8977d38543f7743f12911ecea21
Valdimir Fefilatev <vfefilatyev@express42.com>
2021-06-17 17:23:46 +0300

    HW#6 without wget

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+
+#wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 apt-get update
 apt-get install -y mongodb-org
-
-apt-get update
-apt-get install -y mongodb-org
-
 systemctl start mongod
 systemctl enable mongod
 
