40a526eb753cd3e56b516e6ef12c1a16319e28a9
Valdimir Fefilatev <vfefilatyev@express42.com>
2021-06-19 18:05:55 +0300

    Added HW#8

---
+++ terraform/lb.tf
@@ -0,0 +1,40 @@
+resource "yandex_lb_network_load_balancer" "app" {
+  name = "my-network-load-balancer"
+
+  listener {
+    name = "my-listener"
+    port = 8080
+    target_port = 9292
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.app.id
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 9292
+        path = "/"
+      }
+    }
+  }
+}
+
+resource "yandex_lb_target_group" "app" {
+  name      = "my-target-group"
+  region_id = "ru-central1"
+
+  target {
+    subnet_id = var.subnet_id
+    address   = yandex_compute_instance.app[0].network_interface.0.ip_address
+  }
+
+  target {
+    subnet_id = var.subnet_id
+    address   = yandex_compute_instance.app[1].network_interface.0.ip_address
+  }
+
+}
---
+++ terraform/output.tf
@@ -0,0 +1,26 @@
+# output "external_ip_address_app-0" {
+#   value = yandex_compute_instance.app[0].network_interface.0.nat_ip_address
+# }
+
+# output "external_ip_address_app-1" {
+#   value = yandex_compute_instance.app[1].network_interface.0.nat_ip_address
+# }
+output "external_ip_address_app" {
+  value = [
+    for app in yandex_compute_instance.app:
+      app.network_interface.0.nat_ip_address
+  ]
+}
+
+
+output "external_ip_address_lb" {
+  # value = yandex_lb_network_load_balancer.app.listener.0.external_address_spec.*.address
+  # value = [for s in yandex_lb_network_load_balancer.app.listener: s.external_address_spec.0.address].0
+  value = [
+    for s in yandex_lb_network_load_balancer.app.listener:
+      [
+        for ss in s.external_address_spec:
+          ss.address
+      ]
+    ]
+}
