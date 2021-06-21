88b29e97a07fe13c1b134bc084695080112e4bd8
Valdimir Fefilatev <vfefilatyev@express42.com>
2021-06-21 13:49:45 +0300

    Init terraform-2

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -33,9 +33,9 @@ variable "app_count" {
 }
 variable app_disk_image {
   description = "Disk image for reddit app"
-  default = "reddit-app-base"
+  default     = "reddit-app-base"
 }
 variable db_disk_image {
   description = "Disk image for reddit db"
-  default = "reddit-db-base"
+  default     = "reddit-db-base"
 }
