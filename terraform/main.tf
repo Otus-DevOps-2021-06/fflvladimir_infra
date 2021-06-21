17fa01294a2dc0b8478b7b1a68a8b783c7419e15
Valdimir Fefilatev <vfefilatyev@express42.com>
2021-06-21 13:30:49 +0300

    Remove files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,36 @@
+# terraform {
+#   required_providers {
+#     yandex = {
+#       source = "yandex-cloud/yandex"
+#     }
+#   }
+# }
+
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
+
+  alias = "ya"
 }
 
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
+module "app" {
+  source          = "./modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+  subnet_id       = var.subnet_id
+
+  providers = {
+    yandex = yandex.ya
   }
+}
+
+module "db" {
+  source          = "./modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+  subnet_id       = var.subnet_id
 }
 
 # resource "yandex_compute_instance" "app" {
