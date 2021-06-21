fe93cf3b65a79752f70bfbbdf8ed82e9c8df090a
Valdimir Fefilatev <vfefilatyev@express42.com>
2021-06-21 16:01:19 +0300

    Add storage-bucket.tf

---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,20 @@
+terraform {
+  # https://cloud.yandex.com/en-ru/docs/solutions/infrastructure-management/terraform-state-storage
+  # required_providers {
+  #   yandex = {
+  #     source = "yandex-cloud/yandex"
+  #   }
+  # }
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "fflvladimir"
+    region     = "ru-central1"
+    key        = "terraform.tfstate"
+    # access_key = "<static key identifier>"
+    # secret_key = "<secret key>"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
