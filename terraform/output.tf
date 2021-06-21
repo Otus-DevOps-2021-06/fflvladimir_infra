9612952140c1e46c3dcd73bbb26ef597742db51d
Valdimir Fefilatev <vfefilatyev@express42.com>
2021-06-21 10:16:56 +0300

    Correct output external_lb_address

--- terraform/output.tf
+++ terraform/output.tf
@@ -16,11 +16,16 @@ output "external_ip_address_app" {
 output "external_ip_address_lb" {
   # value = yandex_lb_network_load_balancer.app.listener.0.external_address_spec.*.address
   # value = [for s in yandex_lb_network_load_balancer.app.listener: s.external_address_spec.0.address].0
+
   value = [
     for lis in yandex_lb_network_load_balancer.app.listener :
     [
       for eas in lis.external_address_spec :
       eas.address
-    ]
-  ]
+    ].0
+  ].0
+  # value = [
+  #   for address in yandex_lb_network_load_balancer.app.listener.*.external_address_spec[0] :
+  #   address.address
+  #   ].0
 }
