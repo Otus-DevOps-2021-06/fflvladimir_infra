deb5875950d89a57a193ac38ee22505e230e9ec0
Valdimir Fefilatev <vfefilatyev@express42.com>
2021-06-22 13:16:24 +0300

    Init correct key.json

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -12,8 +12,8 @@ provider "yandex" {
   # service_account_key_file = var.service_account_key_file
 
 
-  service_account_key_file = file("fflvladimir-key.json")
-  # service_account_key_file = file("key.json")
+  # service_account_key_file = file("fflvladimir-key.json")
+  service_account_key_file = file("key.json")
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
