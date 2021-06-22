4d84a998f2306868b250d9d8450cc7311734e6b8
Valdimir Fefilatev <vfefilatyev@express42.com>
2021-06-22 08:52:10 +0300

    Add Readme

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,5 +1,5 @@
 terraform {
-  # https://cloud.yandex.com/en-ru/docs/solutions/infrastructure-management/terraform-state-storage
+  # required_version = "~> 0.12.0"
   # required_providers {
   #   yandex = {
   #     source = "yandex-cloud/yandex"
@@ -8,11 +8,17 @@ terraform {
 
   backend "s3" {
     endpoint   = "storage.yandexcloud.net"
-    bucket     = "fflvladimir"
-    region     = "us-east-1"
+    bucket     = "fflvladimir-2"
+    region     = "ru-central1"
     key        = "terraform.tfstate"
-    # access_key = "<static key identifier>"
-    # secret_key = "<secret key>"
+    # access_key = "key.json"
+    # access_key = "ajen7la5i5naamdghj9g"
+    # access_key = "hzyIbeJpdz6OGyeKM24d"
+    # secret_key = "TOrKiaXf3WrmLJ_UfJLloNoCEyxCz9HYB6e0TG1Y"
+
+    # access_key = "GeP8MYYQlUBf2fCA4IlC"
+    access_key = "hzyIbeJpdz6OGyeKM24d"
+    secret_key = "TOrKiaXf3WrmLJ_UfJLloNoCEyxCz9HYB6e0TG1Y"
 
     skip_region_validation      = true
     skip_credentials_validation = true
